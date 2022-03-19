2fcc5e7f913619376e0f532064f213f8665821cc
nautilus7 <spyros.n7@gmail.com>
2022-03-20 01:44:39 +0200

    [ChoiceList.py] Optimise and improve code
    - Optimise and sort the imports.
    - Vertically center the text on the line.
    - Improve argument processing
    
    Co-authored-by: IanSav <is.ozpvr@gmail.com>
    Co-authored-by: jbleyel <jbleyel@gmx.net>

--- lib/python/Components/ChoiceList.py
+++ lib/python/Components/ChoiceList.py
@@ -1,37 +1,40 @@
-from Components.MenuList import MenuList
-from Tools.Directories import SCOPE_CURRENT_SKIN, resolveFilename
-from enigma import RT_HALIGN_LEFT, eListboxPythonMultiContent, gFont
-from Tools.LoadPixmap import LoadPixmap
+from enigma import RT_HALIGN_LEFT, RT_VALIGN_CENTER, eListboxPythonMultiContent, gFont
+
 from skin import fonts, parameters
+from Components.MenuList import MenuList
+from Tools.Directories import SCOPE_GUISKIN, resolveFilename
+from Tools.LoadPixmap import LoadPixmap
 
 
-def ChoiceEntryComponent(key=None, text=["--"]):
+def ChoiceEntryComponent(key=None, text=None):
+	if text is None:
+		text = ["--"]
 	res = [text]
 	if text[0] == "--":
 		x, y, w, h = parameters.get("ChoicelistDash", (0, 0, 800, 25))
-		res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT, "-" * 200))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, "-" * 200))
 	else:
 		if key:
 			x, y, w, h = parameters.get("ChoicelistName", (45, 0, 800, 25))
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT, text[0]))
+			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, text[0]))
 			if key == "dummy":
 				png = None
 			elif key == "expandable":
-				png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/expandable.png"))
+				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "icons/expandable.png"))
 			elif key == "expanded":
-				png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/expanded.png"))
+				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "icons/expanded.png"))
 			elif key == "verticalline":
-				png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/verticalline.png"))
+				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "icons/verticalline.png"))
 			elif key == "bullet":
-				png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "icons/bullet.png"))
+				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "icons/bullet.png"))
 			else:
-				png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "buttons/key_%s.png" % key))
+				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "buttons/key_%s.png" % key))
 			if png:
 				x, y, w, h = parameters.get("ChoicelistIcon", (5, 0, 35, 25))
 				res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, x, y, w, h, png))
 		else:
 			x, y, w, h = parameters.get("ChoicelistNameSingle", (5, 0, 800, 25))
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT, text[0]))
+			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, text[0]))
 	return res
 
 
