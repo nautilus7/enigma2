20c3e05f8abcd5970385efefd7d0453260cda515
IanSav <IanSav@users.noreply.github.com>
2022-03-29 03:04:12 +1100

    [ChoiceList.py] Clean up and improve the code
    - Widen the default template width to suit the default HD resolution.
    - Make the horizontal line use the UTF-8 dash character rather than "-" (minus) to draw a continuous line.
    - Add a more logical "none" key to do the same as the "dummy" key.
    - Match the default font height to suit the template height.
    - Add "getItemHeight" method.
    - Add ChoicelistIconVerticalline and ChoicelistIconExpanded parameter.  This allows you to define the correct icon positions in skin.

--- lib/python/Components/ChoiceList.py
+++ lib/python/Components/ChoiceList.py
@@ -11,13 +11,13 @@ def ChoiceEntryComponent(key=None, text=None):
 		text = ["--"]
 	res = [text]
 	if text[0] == "--":
-		x, y, w, h = parameters.get("ChoicelistDash", (0, 0, 800, 25))
-		res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, "-" * 200))
+		x, y, w, h = parameters.get("ChoicelistDash", (0, 0, 1280, 25))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, "\u2014" * 200))
 	else:
 		if key:
-			x, y, w, h = parameters.get("ChoicelistName", (45, 0, 800, 25))
+			x, y, w, h = parameters.get("ChoicelistName", (45, 0, 1235, 25))
 			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, text[0]))
-			if key == "dummy":
+			if key in ("dummy", "none"):
 				png = None
 			elif key == "expandable":
 				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "icons/expandable.png"))
@@ -31,9 +31,13 @@ def ChoiceEntryComponent(key=None, text=None):
 				png = LoadPixmap(resolveFilename(SCOPE_GUISKIN, "buttons/key_%s.png" % key))
 			if png:
 				x, y, w, h = parameters.get("ChoicelistIcon", (5, 0, 35, 25))
+				if key == "verticalline" and "ChoicelistIconVerticalline" in parameters:
+					x, y, w, h = parameters.get("ChoicelistIconVerticalline", (5, 0, 35, 25))
+				if key == "expanded" and "ChoicelistIconExpanded" in parameters:
+					x, y, w, h = parameters.get("ChoicelistIconExpanded", (5, 0, 35, 25))
 				res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, x, y, w, h, png))
 		else:
-			x, y, w, h = parameters.get("ChoicelistNameSingle", (5, 0, 800, 25))
+			x, y, w, h = parameters.get("ChoicelistNameSingle", (5, 0, 1275, 25))
 			res.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, text[0]))
 	return res
 
@@ -41,12 +45,16 @@ def ChoiceEntryComponent(key=None, text=None):
 class ChoiceList(MenuList):
 	def __init__(self, list, selection=0, enableWrapAround=False):
 		MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
-		font = fonts.get("ChoiceList", ("Regular", 20, 30))
+		font = fonts.get("ChoiceList", ("Regular", 20, 25))
 		self.l.setFont(0, gFont(font[0], font[1]))
 		self.l.setItemHeight(font[2])
+		self.itemHeight = font[2]
 		self.selection = selection
 
 	def postWidgetCreate(self, instance):
 		MenuList.postWidgetCreate(self, instance)
 		self.moveToIndex(self.selection)
 		self.instance.setWrapAround(True)
+
+	def getItemHeight(self):
+		return self.itemHeight
