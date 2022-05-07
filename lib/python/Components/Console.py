433d662c8a1616f9945744b94d2c5ec54b16ecf0
nautilus7 <spyros.n7@gmail.com>
2022-05-07 16:31:53 +0300

    [Console.py] Refactor code
    Cumulative changes from IanSav's refactor. Key changes include:
    
    - Fix logic to properly allow commands submitted as an argument list as well as simple text command lines.
    - Sort and optimise imports.
    - Log all commands being executed.
    - Use camelCase variables.
    - Improve logging text.
    - Fix bug in extra_args initialisation.  The extra_args argument is optional and *not* always a list.
    - Improve logging and add more information to the logs when the code is waiting for a spawned process to finish.
    - Rename finishedCB to appClosedCB to better batch the code for dataAvailCB.
    
    Co-authored-by: IanSav <IanSav@users.noreply.github.com>

--- lib/python/Components/Console.py
+++ lib/python/Components/Console.py
@@ -1,52 +1,54 @@
-import enigma
-import ctypes
-import os
+from os import waitpid
+
+from enigma import eConsoleAppContainer
 
 
 class ConsoleItem:
-	def __init__(self, containers, cmd, callback, extra_args, binary=False):
-		self.extra_args = extra_args
-		self.callback = callback
-		self.container = enigma.eConsoleAppContainer()
+	def __init__(self, containers, cmd, callback, extraArgs, binary=False):
 		self.containers = containers
-		self.binary = binary
-		# Create a unique name
-		name = cmd
-		if name in containers:
-			name = str(cmd) + '@' + hex(id(self))
+		if isinstance(cmd, str):  # Until .execute supports a better API.
+			cmd = [cmd]
+		self.callback = callback
+		self.extraArgs = extraArgs
+		name = str(cmd)
+		if name in self.containers:
+			name = "%s@%s" % (name, hex(id(self)))  # Create a unique name.
+		self.containers[name] = self
 		self.name = name
-		containers[name] = self
-		# If the caller isn't interested in our results, we don't need
-		# to store the output either.
-		if callback is not None:
+		self.container = eConsoleAppContainer()
+		self.binary = binary
+		if callback is not None:  # If the caller isn't interested in our results, we don't need to store the output either.
 			self.appResults = []
 			self.container.dataAvail.append(self.dataAvailCB)
-		self.container.appClosed.append(self.finishedCB)
-		if isinstance(cmd, str): # until .execute supports a better api
-			cmd = [cmd]
-		retval = self.container.execute(*cmd)
-		if retval:
-			self.finishedCB(retval)
+		self.container.appClosed.append(self.appClosedCB)
+		if len(cmd) > 1:
+			print("[Console] Processing command '%s' with arguments '%s'." % (cmd[0], "', '".join(cmd[1:])))
+		else:
+			print("[Console] Processing command line '%s'." % cmd[0])
+		retVal = self.container.execute(*cmd)
+		if retVal:
+			self.appClosedCB(retVal)
 		if callback is None:
 			try:
-				os.waitpid(self.container.getPID(), 0)
-			except:
-				pass
+				pid = self.container.getPID()
+				print("[Console] Waiting for command (PID %d) to finish." % pid)
+				pid, exitVal = waitpid(pid, 0)
+			except (IOError, OSError) as err:
+				print("[Console] Error %s: Wait for command to terminate failed!  (%s)" % (err.errno, err.strerror))
 
 	def dataAvailCB(self, data):
 		self.appResults.append(data)
 
-	def finishedCB(self, retval):
-		print("[Console] finished:", self.name)
+	def appClosedCB(self, retVal):
+		print("[Console] Command '%s' finished with exit status of %d." % (self.name, retVal))
 		del self.containers[self.name]
 		del self.container.dataAvail[:]
 		del self.container.appClosed[:]
 		self.container = None
-		callback = self.callback
-		if callback is not None:
-			data = b''.join(self.appResults)
-			data = data if self.binary else data.decode()
-			callback(data, retval, self.extra_args)
+		if self.callback is not None:
+			appResults = b"".join(self.appResults)
+			appResults = appResults if self.binary else appResults.decode()
+			self.callback(appResults, retVal, self.extraArgs)
 
 
 class Console:
@@ -54,37 +56,38 @@ class Console:
 		Console by default will work with strings on callback.
 		If binary data required class shoud be initialized with Console(binary=True)
 	"""
+
 	def __init__(self, binary=False):
-		# Still called appContainers because Network.py accesses it to
-		# know if there's still stuff running
+		# Still called appContainers because Network.py, SoftwareTools.py
+		# and WirelessLan/Wlan.py accesses it to know if there's still
+		# stuff running.
 		self.appContainers = {}
 		self.binary = binary
 
-	def ePopen(self, cmd, callback=None, extra_args=[]):
-		print("[Console] command:", cmd)
+	def ePopen(self, cmd, callback=None, extra_args=None):
 		return ConsoleItem(self.appContainers, cmd, callback, extra_args, self.binary)
 
-	def eBatch(self, cmds, callback, extra_args=[], debug=False):
+	def eBatch(self, cmds, callback, extra_args=None, debug=False):
 		self.debug = debug
 		cmd = cmds.pop(0)
 		self.ePopen(cmd, self.eBatchCB, [cmds, callback, extra_args])
 
-	def eBatchCB(self, data, retval, _extra_args):
-		(cmds, callback, extra_args) = _extra_args
+	def eBatchCB(self, data, retVal, extraArg):
+		(cmds, callback, extraArgs) = extraArg
 		if self.debug:
-			print('[eBatch] retval=%s, cmds left=%d, data:\n%s' % (retval, len(cmds), data))
+			print("[Console] eBatch DEBUG: retVal=%s, cmds left=%d, data:\n%s" % (retVal, len(cmds), data))
 		if cmds:
 			cmd = cmds.pop(0)
-			self.ePopen(cmd, self.eBatchCB, [cmds, callback, extra_args])
+			self.ePopen(cmd, self.eBatchCB, [cmds, callback, extraArgs])
 		else:
-			callback(extra_args)
+			callback(extraArgs)
 
 	def kill(self, name):
 		if name in self.appContainers:
-			print("[Console] killing: ", name)
+			print("[Console] Killing command '%s'." % name)
 			self.appContainers[name].container.kill()
 
 	def killAll(self):
 		for name, item in self.appContainers.items():
-			print("[Console] killing: ", name)
+			print("[Console] Killing all commands '%s'." % name)
 			item.container.kill()
